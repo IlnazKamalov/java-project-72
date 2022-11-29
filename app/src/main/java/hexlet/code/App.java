bd31d4b31832b1b257371565fdb4431f7ad3bc20
IlnazKamalov <kamalovpk@gmail.com>
2022-11-29 17:41:30 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(1313);
+        app.start(5000);
     }
 
     public static Javalin getApp() {
