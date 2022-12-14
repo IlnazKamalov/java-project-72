890ff2d4bfb33cb7667e0cc8a728a6a341d70129
IlnazKamalov <kamalovpk@gmail.com>
2022-12-14 13:41:05 +0300

    add tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,50 +3,48 @@ package hexlet.code;
 import io.ebean.DB;
 import io.ebean.Transaction;
 import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class AppTest {
 
     private static Javalin app;
-    private static String url;
+    private static String baseUrl;
     private static Transaction transaction;
-    private static MockWebServer server1;
-    private static MockWebServer server2;
+    private static MockWebServer server;
+    private static final int OK = 200;
+    private static final int REDIRECT = 302;
 
     @BeforeAll
     public static void beforeAll() throws IOException {
         app = App.getApp();
         app.start(0);
         int port = app.port();
-        url = "http://localhost:" + port;
+        baseUrl = "http://localhost:" + port;
 
-        server1 = new MockWebServer();
-        server2 = new MockWebServer();
-
+        server = new MockWebServer();
         String pageAll = Files.readString(Path.of("src/test/resources/test1.html"));
-        String pageSome = Files.readString(Path.of("src/test/resources/test2.html"));
-
-        server1.enqueue(new MockResponse().setBody(pageAll));
-        server2.enqueue(new MockResponse().setBody(pageSome));
-
-        server1.start();
-        server2.start();
+        server.enqueue(new MockResponse().setBody(pageAll));
+        server.start();
     }
 
     @AfterAll
     public static void afterAll() throws IOException {
         app.stop();
-        server1.shutdown();
-        server2.shutdown();
+        server.shutdown();
     }
 
     @BeforeEach
@@ -57,5 +55,108 @@ class AppTest {
     @AfterEach
     void afterEach() {
         transaction.rollback();
+    }
+
+    @Test
+    void testRoot() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl)
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(OK);
+        assertThat(response.getBody()).contains("Анализатор страниц");
+    }
+
+    @Test
+    void testRootURLS() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(OK);
+        assertThat(response.getBody()).contains("Page analyzer");
+    }
+
+    @Test
+    void testUrl() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls/2")
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(OK);
+        assertThat(response.getBody()).contains("https://www.railway.app");
+    }
+
+    @Test
+    void testUrls() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(OK);
+        assertThat(response.getBody()).contains("https://www.github.com");
+        assertThat(response.getBody()).contains("https://www.railway.app");
+    }
+
+    @Test
+    void testRedirectUrl() {
+        final String testRedirect = "https://www.youtube.com/c/google";
+
+        HttpResponse response = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", testRedirect)
+                .asEmpty();
+
+        assertThat(response.getStatus()).isEqualTo(REDIRECT);
+        assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/urls");
+    }
+
+    @Test
+    void testSuccessAddUrlAndPageAlreadyExist() {
+        final String testAdd = "https://www.youtube.com";
+        final String parsedTestAdd = "www.youtube.com";
+
+        Unirest
+                .post(baseUrl + "/urls")
+                .field("url", testAdd)
+                .asEmpty();
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+
+        assertThat(response.getBody()).contains(parsedTestAdd);
+        assertThat(response.getBody()).contains("Страница успешно добавлена");
+
+        Unirest
+                .post(baseUrl + "/urls")
+                .field("url", testAdd)
+                .asEmpty();
+
+        HttpResponse<String> responseWithAlreadyAdd = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+        String body02 = responseWithAlreadyAdd.getBody();
+
+        assertThat(body02).contains("Страница уже существует");
+        assertThat(body02).contains(parsedTestAdd);
+    }
+
+    @Test
+    void testIncorrectAddUrl() {
+        final String incorrectUrl = "trompompom.com";
+
+        HttpResponse<String> response = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", incorrectUrl)
+                .asString();
+
+        assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/");
+
+        HttpResponse<String> response1 = Unirest
+                .get(baseUrl)
+                .asString();
+
+        assertThat(response1.getBody()).contains("Некорректный URL");
     }
 }
--- app/src/test/resources/seed.sql
+++ app/src/test/resources/seed.sql
@@ -1,4 +1,4 @@
 INSERT INTO url (name, created_at)
 VALUES
-    ('https://github.com', '2022-11-28 09:00:00'),
-    ('https://railway.app', '2022-12-2 17:00:00');
+    ('https://www.github.com', '2022-11-28 09:00:00'),
+    ('https://www.railway.app', '2022-12-2 17:00:00');
