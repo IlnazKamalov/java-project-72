2c245343aa9edf2ce655dbed2c621e40f93bb631
IlnazKamalov <kamalovpk@gmail.com>
2022-12-15 16:53:53 +0300

    fix linter

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,7 +7,11 @@ import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -136,5 +140,23 @@ class AppTest {
 
         assertThat(body).contains("Страница уже существует");
         assertThat(body).contains(parsedTestAdd);
+    }
+
+    @Test
+    void testIncorrectAddUrl() {
+        final String incorrectUrl = "trompompom.com";
+
+        HttpResponse<String> response = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", incorrectUrl)
+                .asString();
+
+        assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/");
+
+        HttpResponse<String> response1 = Unirest
+                .get(baseUrl)
+                .asString();
+
+        assertThat(response1.getBody()).contains("Некорректный URL");
     }
 }
